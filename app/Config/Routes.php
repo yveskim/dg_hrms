282db4266d4b695a99ed87303030d03e3cbee17b
Yves Kim Cabanting <yveskim.cabanting@iloilonhs.edu.ph>
2023-11-14 10:29:55 +0800

    second clean up

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -47,20 +47,13 @@ $routes->get('jhs_program', 'Academics::jhsProgram');
 
 //LOGIN ROUTES
 $routes->post('login/getEmpData', 'Login::getEmpData'); //load the data in modal in registering new emp page
-$routes->post('updateStudentPassword', 'Login::updateStudentPassword');
 $routes->get('signOutAdmin', 'Admin::sign_out');
-$routes->get('signOutStudent', 'Admin::signOutStudent');
 $routes->post('sign_out', 'Dashboard::sign_out');
 $routes->match(['get', 'post'], 'login_admin', 'Login::index', ['filter' => 'checkLogout']);
-$routes->match(['get', 'post'], 'login_student', 'Login::loginStudent', ['filter' => 'checkLogout']);
 $routes->match(['get', 'post'], 'validate', 'Login::validation');
 // $routes->get('administrator_login', 'Dashboard::index', ['filter' => 'checkLogin']);
 $routes->get('login_application', 'ApplicantProfile::index', ['filter' => 'checkLogin']);
-$routes->get('change_password', 'ApplicantProfile::changePass');
-$routes->post('updatePassword', 'ApplicantProfile::updatePassword');
 $routes->match(['get', 'post'], 'register', 'Login::register');
-$routes->get('logout_user', 'ApplicantProfile::log_out');
-$routes->get('student_login', 'Login::student_profile');
 
 // RESET PASSWORD USERS
 $routes->post('reset_password_info', 'Users::reset_password_users');
@@ -167,49 +160,6 @@ $routes->post('employee/updateProfile', 'Faculty::updateProfile');
 
 
 
-
-//STUDENT
-// $routes->get('student', 'Student::index');
-$routes->get('student', 'Student::index', ['filter' => 'checkLoginStudent']);
-$routes->match(['get', 'post'], 'validate_student', 'Login::validateStudent');
-$routes->post('updateStudentInfo', 'Student::updateStudentInfo');
-$routes->post('updateStudImage', 'Student::updateStudImage');
-$routes->get('getGradeLevelJhs', 'Student::getGradeLevelJhs');
-$routes->get('getGradeLevelShs', 'Student::getGradeLevelShs');
-$routes->get('getSemester', 'Student::getSemester');
-$routes->get('editEnStatus', 'Student::editEnStatus');
-$routes->post('updateEnStat', 'Student::updateEnStat');
-
-//STUDENT ADDRESS
-$routes->post('setStudentAddress', 'Student::setStudentAddress');
-$routes->get('getStudentAddress', 'Student::getStudentAddress');
-$routes->get('getAddress', 'Student::getAddress');
-$routes->get('deleteAddress', 'Student::deleteAddress');
-$routes->get('loadFamilyBg', 'Student::loadFamilyBg');
-$routes->post('updateFamilyBg', 'Student::updateFamilyBg');
-$routes->get('getStudentDevices', 'Student::getStudentDevices');
-$routes->post('setStudentDevice', 'Student::setStudentDevice');
-$routes->get('getDevices', 'Student::getDevices');
-$routes->get('deleteDevice', 'Student::deleteDevice');
-
-// STUDENT HEALTH
-$routes->get('getStudentHealth', 'Student::getStudentHealth');
-$routes->post('setStudentHealth', 'Student::setStudentHealth');
-$routes->get('getHealth', 'Student::getHealth');
-$routes->get('deleteHealth', 'Student::deleteHealth');
-
-// STUDENT SCHOOL
-$routes->get('getStudentSchool', 'Student::getStudentSchool');
-$routes->post('setStudentSchool', 'Student::setStudentSchool');
-$routes->get('getSchool', 'Student::getSchool');
-$routes->get('deleteSchool', 'Student::deleteSchool');
-
-// STUDENT HOUSEHOLD
-$routes->get('getStudentHm', 'Student::getStudentHm');
-$routes->post('setStudentHm', 'Student::setStudentHm');
-$routes->get('getHm', 'Student::getHm');
-$routes->get('deleteHm', 'Student::deleteHm');
-
 //EMPLOYEE ROUTE
 $routes->get('getAllActiveEmpNoPlantilla', 'Employee::getAllActiveEmpNoPlantilla');
 
@@ -243,13 +193,6 @@ $routes->get('application', 'Admission::application');
 $routes->post('admission/verify_captcha', 'Admission::captcha_verify');
 $routes->get('admission/loadAdmission', 'Admission::loadAdmission');
 
-//$routes->get('admission/loadPending', 'Admission::loadPending');
-$routes->post('admission/acceptRequest', 'Admission::acceptRequest');
-$routes->post('admission/saveAdmission', 'Admission::saveAdmission');
-$routes->post('admission/update_info', 'Admission::UpdateAdmissionInfo');
-
-$routes->get('getStudentUserInfo', 'Admission::getStudentUserInfo');
-
 $routes->get('sendEmail', 'EmailController::index');
 
 //NEWS ROUTE
@@ -269,35 +212,6 @@ $routes->get('news_spbe', 'News::news_spbe');
 
 $routes->get('news/(:segment)', 'News::view/$1');
 
-//APPLICATION ROUTES
-$routes->get('editApplication', 'Application::editApplication');
-$routes->post('update_app_form', 'Application::updateApplication');
-$routes->post('delete_application', 'Application::deleteApplication');
-$routes->get('getAppForm', 'Application::getForm');
-$routes->get('getAppFormTransferees', 'Application::getTransfereesForm');
-$routes->get('loadExistingTransaction', 'Application::loadExistingTransaction');
-$routes->get('getApp/getAll', 'Application::getAllApplication');
-$routes->get('getApp/getValidatedApp', 'Application::getAllValidatedApp');
-$routes->get('getAppDetails', 'Application::getAppDetails');
-$routes->post('saveExamScore', 'Application::saveExamScore');
-$routes->post('checkAppFiles', 'Application::checkApplicationFiles');
-$routes->get('print_application', 'Application::printApplication');
-
-$routes->post('submit_app_form', 'Application::submitApplication');
-$routes->post('submit_transferee_form', 'Application::submitTransfereeForm');
-
-//UPDATE TRANSFEREE
-$routes->get('editTransferee', 'Application::editTransferee');
-$routes->post('updateTransferee', 'Application::updateTransferee');
-$routes->post('delete_transferee', 'Application::deleteTransferee');
-$routes->get('print_application_transferee', 'Transferees::printApplicationTransferee');
-
-//UPDATE Application
-$routes->get('editAppStem', 'Application::editApplicationStem');
-$routes->get('getAppUpdatedData', 'Application::getAppUpdatedData');
-$routes->get('getUpdateHistory', 'Application::getUpdateHistory');
-$routes->post('updateAppStem', 'Application::updateAppStem');
-
 //REPORT ROUTES
 $routes->get('report/pds_pdf', 'Pdf_report::generated_pds');
 
@@ -307,29 +221,6 @@ $routes->get('getSchoolYear', 'Enrollment::getSchoolYear');
 $routes->post('encode_student', 'Enrollment::encodeStudent');
 $routes->get('getStudentData', 'Enrollment::getStudentData');
 $routes->get('getStudentDataByUser', 'Enrollment::getStudentDataByUser');
-
-$routes->get('getEnrolledData', 'Enrollment::getEnrolledData');
-$routes->get('getEnrolledJhsData', 'Enrollment::getEnrolledJhsData');
-$routes->get('getEnrolledShsData', 'Enrollment::getEnrolledShsData');
-$routes->get('getEnrolledByUser', 'Enrollment::getEnrolledByUser');
-$routes->get('unenrollStudent', 'Enrollment::unenrollStudent');
-$routes->get('getAverageRecord', 'Enrollment::getAverageRecord');
-$routes->post('setAverageRecord', 'Enrollment::setAverageRecord');
-$routes->get('getStudentGrade', 'Enrollment::getStudentGrade');
-$routes->get('deleteGrade', 'Enrollment::deleteGrade');
-
-$routes->get('getEnrolledDataNoProgram', 'Enrollment::getEnrolledDataNoProgram');
-
-$routes->get('deleteEnrollee', 'Enrollment::deleteEnrollee');
-$routes->get('getSearchId', 'Enrollment::searchStudentId');
-$routes->get('getSearchName', 'Enrollment::searchStudentName');
-$routes->get('getSearchData', 'Enrollment::getSearchData');
-$routes->post('enrollStudent', 'Enrollment::enrollStudent');
-$routes->get('loadEnrolleeProfile', 'Enrollment::loadEnrolleeProfile');
-$routes->get('loadEnrolleeInfo', 'Enrollment::loadEnrolleeInfo');
-
-// generate RF +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-$routes->get('generateEF', 'Enrollment::generateEF');
 
 //FACULTY ROUTES
 
@@ -348,39 +239,6 @@ $routes->get('getTeacherNoDepartment', 'Department::getTeacherNoDepartment');
 $routes->get('getDepartmentTeachers', 'Department::getDepartmentTeachers');
 $routes->post('addTeacherToDepartment', 'Department::addTeacherToDepartment');
 
-//PROGRAM ROUTES
-$routes->get('getSchoolProgram', 'Program::getProgramDetails');
-$routes->get('getProgramAdvisery', 'Program::getProgramAdvisery');
-$routes->get('getAdviseryDetails', 'Program::getAdviseryDetails');
-$routes->post('addAdvisery', 'Program::addAdvisery');
-$routes->post('addSection', 'Program::addSection');
-$routes->get('getGradeLevel', 'Program::getGradeLevel');
-$routes->get('getCategory', 'Program::getCategory');
-$routes->get('getSections', 'Program::getSections');
-
-$routes->post('changeProgramLogo', 'Program::changeProgramLogo');
-
-$routes->get('getTeacherNoProgram', 'Program::getTeacherNoProgram');
-$routes->post('addTeacherToProgram', 'Program::addTeacherToProgram');
-
-$routes->get('getProgramDetails', 'Program::getProgramDetails');
-$routes->get('getProgramTeachers', 'Program::getProgramTeachers');
-$routes->get('getStudentInProgram', 'Program::getStudentInProgram');
-$routes->post('addStudentToProgram', 'Program::addStudentToProgram');
-$routes->get('removeStudentFromProgram', 'Program::removeStudentFromProgram');
-$routes->post('assignProgramHead', 'Program::assignProgramHead');
-
-//SECTIONS
-$routes->get('getEachSection', 'Sections::getEachSection');
-
-$routes->get('getSectionStudents', 'Sections::getSectionStudents');
-$routes->get('getStudentNotAssignedInSection', 'Sections::getStudentNotAssignedInSection');
-$routes->post('addStudentToSection', 'Sections::addStudentToSection');
-$routes->get('getSectionStudentStrand', 'Sections::getSectionStudentStrand');
-$routes->get('getEachSectionStrand', 'Sections::getEachSectionStrand');
-$routes->get('removeFromSection', 'Sections::removeFromSection');
-$routes->get('removeFromSectionShs', 'Sections::removeFromSectionShs');
-
 //PLANTILLA
 $routes->get('getAllPlantilla', 'Plantilla::getAllPlantilla');
 
@@ -390,37 +248,6 @@ $routes->get('getPrograms', 'Settings::getPrograms');
 $routes->post('setProgram', 'Settings::setProgram');
 $routes->post('setActiveSy', 'Settings::setActiveSy');
 $routes->post('setActiveSemester', 'Settings::setActiveSemester');
-
-// SUBJECT GROUP ROUTE
-
-$routes->get('getSg', 'SubjectGroup::getSg');
-$routes->get('getSgInfo', 'SubjectGroup::getSgInfo');
-$routes->get('getFacultySg', 'SubjectGroup::getFacultySg');
-$routes->post('addTeacherToSg', 'SubjectGroup::addTeacherToSg');
-$routes->get('getSgTeachers', 'SubjectGroup::getSgTeachers');
-
-// TRACK ROUTE
-$routes->get('getTrack', 'Track::getTrack');
-$routes->get('getTrackDetails', 'Track::getTrackDetails');
-$routes->get('getStrand', 'Track::getStrand');
-
-// STRANDS ROUTE
-$routes->get('getStrandDetails', 'Strand::getStrandDetails');
-$routes->get('getEnrolledDataNoStrand', 'Strand::getEnrolledDataNoStrand');
-$routes->post('addStudentToStrand', 'Strand::addStudentToStrand');
-$routes->get('getStudentInStrand', 'Strand::getStudentInStrand');
-$routes->get('getStrandSections', 'Strand::getStrandSections');
-$routes->get('getStrandAdvisery', 'Strand::getStrandAdvisery');
-$routes->post('addSectionStrand', 'Strand::addSectionStrand');
-$routes->post('addStudentToStrandSection', 'Strand::addStudentToStrandSection');
-$routes->get('getStudentInStrandNoSection', 'Strand::getStudentInStrandNoSection');
-$routes->get('getStrandAdviseryDetails', 'Strand::getStrandAdviseryDetails');
-$routes->post('setAdviseryShs', 'Strand::setAdviseryShs');
-$routes->get('removeStudentFromStrand', 'Strand::removeStudentFromStrand');
-
-$routes->get('getSectionSubjects', 'Strand::getSectionSubjects');
-$routes->get('getShsSubjects', 'Strand::getShsSubjects');
-$routes->post('addSubjectToSection', 'Strand::addSubjectToSection');
 
 // ________________________________________________
 // ________________________________________________
